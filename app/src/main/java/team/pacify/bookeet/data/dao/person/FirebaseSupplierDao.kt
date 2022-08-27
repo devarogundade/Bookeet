03efd76d79f24970f59f18a06440ceaad5585df3
Justin Chinedu <chinedujustin491@gmail.com>
2022-08-27 07:50:23 +0000

    Added supplier Dao

--- app/src/main/java/team/pacify/bookeet/data/dao/person/FirebaseSupplierDao.kt
+++ app/src/main/java/team/pacify/bookeet/data/dao/person/FirebaseSupplierDao.kt
@@ -1,6 +1,7 @@
 package team.pacify.bookeet.data.dao.person
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.toObject
 import kotlinx.coroutines.tasks.await
 import team.pacify.bookeet.data.models.person.Supplier
@@ -9,20 +10,35 @@ import javax.inject.Inject
 
 class FirebaseSupplierDao @Inject constructor(
     private val fStore: FirebaseFirestore,
-) {
+) : SupplierDao {
 
-    suspend fun getSupplier(supplierId: String): Supplier {
+    override suspend fun getSupplier(supplierId: String): Supplier {
         val doc = fStore.collection(DbConstants.SUPPLIERS_PATH)
             .document(supplierId).get().await()
         val supplier = doc.toObject<Supplier>()
         return supplier ?: throw Exception("No supplier found with ID")
     }
 
-    suspend fun getAllSuppliersByLocation(userId: String): List<Supplier> {
+    override suspend fun getAllSuppliersByState(state: String): List<Supplier> {
         val doc = fStore.collection(DbConstants.SUPPLIERS_PATH)
-        val query = doc.whereEqualTo("userId", userId)
-        val entries = ArrayList<Supplier>(1)
+        val query = doc.whereEqualTo("location.state", state)
         val result = query.get().await()
+
+        return fetchResultsFromQuery(result)
+    }
+
+    override suspend fun getAllSuppliersByLga(lga: String): List<Supplier> {
+        val doc = fStore.collection(DbConstants.SUPPLIERS_PATH)
+        val query = doc.whereEqualTo("location.lga", lga)
+        val result = query.get().await()
+
+        return fetchResultsFromQuery(result)
+    }
+
+    private fun fetchResultsFromQuery(
+        result: QuerySnapshot,
+    ): ArrayList<Supplier> {
+        val entries = ArrayList<Supplier>(1)
 
         for (r in result.documents) {
             val supplier = r.toObject<Supplier>()
